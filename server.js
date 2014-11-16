e72f509da6e8bd0d9638597479edfa7ed0b64ce6
Alan Liang <alantliang@gmail.com>
2014-11-16 13:53:10 -0500

    static index page

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	res.send("Hello World!");
+  // res.render("public/views/index.html");
 })
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+  console.log('Example app listening at http://%s:%s', host, port);
+});
