83844abc69efbd771128cb55a2e71037c7c85041
Alan Liang <alantliang@gmail.com>
2014-11-18 21:40:54 -0500

    fixed static path to html file

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-	res.send("Hello World!");
+	res.sendfile(path.join(__dirname, "/public/views/index.html"));
   // res.render("public/views/index.html");
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address;
   var port = server.address().port;
